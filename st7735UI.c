dac3a31a2d53f3dbec44475eb859b67d3302fab1
Leo Tovarys <leosekt@gmail.com>
2020-10-16 17:00:33 +0200

    adding show_screen() to UI

--- st7735UI.c
+++ st7735UI.c
@@ -1,15 +1,30 @@
 #include "st7735UI.h"
 #include "st7735.h"
 
-void view_temperature( Temperature *fst, Temperature *snd ) {
-    for ( int i = 0; i < 7; ++i) {
-        draw( 10 + ( i * 16 ), 10, fst->name[ i ], 0xF800, 2 );
-        draw( 10 + ( i * 16 ), 64, snd->name[ i ], 0xF800, 2 );
-        
-        if ( i == 6 )
-            break;
-        
-        draw( 10 + ( i * 17 ), 35, fst->real_temp[ i ], 0x07FF, 2 );
-        draw( 10 + ( i * 17 ), 89, snd->real_temp[ i ], 0x07FF, 2 );
+void show_name( Temperature *temperature, int x, int y, int clear ) {
+    for ( int i = 0; i < 8; ++i) { 
+        draw( x + ( i * 18 ), y, temperature->name[ i ], clear ? 0x0000 : 0x07FF, 3 );
     }
 }
+
+void show_temperature( Temperature *temperature, int x, int y, int clear ) {
+    for ( int i = 0; i < 5; ++i) { 
+        draw( x + ( i * 22 ), y, temperature->real_temp[ i ], clear ? 0x0000 : 0x07FF, 3 );
+    }
+    
+}
+
+void show_screen( Screen *screen ) {
+    show_name( screen->temp, 0, 5, 0 );
+    show_temperature( screen->temp, 15, 57, 0 );
+    /* drawing underlines for numbers */
+    if ( screen->mode == 1 ) {
+        if ( screen->align == 0 )
+            draw( 15 + 22 * ( screen->align + 3 ), 62, 95, 0x0000, 3 );
+        else
+            draw( 15 + 22 * ( screen->align - 1 ), 62, 95, 0x0000, 3 );
+        draw( 15 + 22 * ( screen->align ), 62, 95, 0xFFE0, 3 );
+    }    
+}
+
+
--- st7735UI.h
+++ st7735UI.h
@@ -1,6 +1,6 @@
 /* 
  * File:  st7735UI.h 
- * Author: Leo Tovary?
+ * Author: Leo Tovarys
  * Comments:
  * Revision history: 
  */
@@ -11,14 +11,25 @@
 #include <xc.h> // include processor files - each processor file is guarded. 
 
 typedef struct Temperature {
-    char name[8];
-    unsigned char real_temp[6];
-    unsigned char set_temp[6];
+    char name[ 8 ];
+    unsigned char real_temp[ 5 ];
+    unsigned char set_temp[ 5 ];
 } Temperature; 
 
-void view_temperature( Temperature *, Temperature * );
+typedef struct Screen {
+    Temperature *temp;
+    int id;
+    int mode;
+    int align;
+} Screen;
+
+void show_screen( Screen * );
+
+void show_temperature( Temperature *, int, int, int );
+
+void show_name( Temperature *, int, int, int );
 
 void set_temperature( Temperature * );
 
-#endif	/* XC_HEADER_TEMPLATE_H */
+#endif
 
